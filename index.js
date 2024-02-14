efd4f99b848fa679fcaa316cd49e27fb310a7e8e
Vayo <ayomipo185@gmail.com>
2024-02-14 14:49:30 +0100

    Switch to Fischer-Yates algorithm for randomization.

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
 const input = document.querySelector(".input");
 const output = document.querySelector(".output > h3");
+const itemListElements = document.querySelectorAll("ul > li");
 
-document.addEventListener("click", function(e) {
-  if (e.target.classList.contains("start") || e.target.classList.contains("skip")) {
-    startRandomizer(e);
+let itemList = [];
+let currentIndex = 0;
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function shuffleItemList() {
+  itemList = Array.from(itemListElements)
+    .map((item) => item.textContent)
+    .filter((item) => item !== "");
+
+  shuffle(itemList);
+}
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("start")) {
+    shuffleItemList();
+    updateOutput();
+  } else if (e.target.classList.contains("skip")) {
+    currentIndex++;
+    updateOutput();
   } else if (e.target.classList.contains("accept")) {
     accept();
   }
 });
 
-let itemList;
-let randomItem;
-
-function startRandomizer(e) {
-  if (e !== undefined && e.target.classList.contains("start")) {
-    itemList = Array.from(document.querySelectorAll("ul > li"))
-      .map((item) => item.textContent)
-      .filter((item) => item !== "");
+function accept() {
+  if (itemList.length === 0) {
+    alert("No more items left");
+    return;
   }
+  itemList.splice(currentIndex, 1);
+  updateOutput();
+}
 
-  randomItem = itemList[Math.floor(Math.random() * itemList.length)];
+function updateOutput() {
+  if (currentIndex >= itemList.length) {
+    shuffle(itemList);
+    currentIndex = 0;
+  }
+  const randomItem = itemList[currentIndex];
   output.textContent = randomItem;
   input.style.display = "none";
   document.querySelector(".output").style.display = "flex";
-}
-
-function accept() {
-  itemList = itemList.filter((item) => item !== randomItem);
-  startRandomizer();
 }
 
